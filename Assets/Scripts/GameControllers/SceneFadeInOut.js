ae99feccbdfb635e5fb73986a260df385d9dac64
pokka <ifunafu@gmail.com>
2013-08-11 04:04:56 +0800

    Screen Fader
    Control the game level's swtiching effect of the screen

---
+++ Assets/Scripts/GameControllers/SceneFadeInOut.js
@@ -0,0 +1,69 @@
+﻿#pragma strict
+
+public var fadeSpeed : float = 1.5f;            // Speed that the screen fades to and from black.
+
+
+private var sceneStarting : boolean = true;     // Whether or not the scene is still fading in.
+
+
+function Awake ()
+{
+    // Set the texture so that it is the the size of the screen and covers it.
+    guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+}
+
+
+function Update ()
+{
+    // If the scene is starting...
+    if(sceneStarting)
+        // ... call the StartScene function.
+        StartScene();
+}
+
+
+function FadeToClear ()
+{
+    // Lerp the colour of the texture between itself and transparent.
+    guiTexture.color = Color.Lerp(guiTexture.color, Color.clear, fadeSpeed * Time.deltaTime);
+}
+
+
+function FadeToBlack ()
+{
+    // Lerp the colour of the texture between itself and black.
+    guiTexture.color = Color.Lerp(guiTexture.color, Color.black, fadeSpeed * Time.deltaTime);
+}
+
+
+function StartScene ()
+{
+    // Fade the texture to clear.
+    FadeToClear();
+
+    // If the texture is almost clear...
+    if(guiTexture.color.a <= 0.05f)
+    {
+        // ... set the colour to clear and disable the GUITexture.
+        guiTexture.color = Color.clear;
+        guiTexture.enabled = false;
+
+        // The scene is no longer starting.
+        sceneStarting = false;
+    }
+}
+
+
+public function EndScene ()
+{
+    // Make sure the texture is enabled.
+    guiTexture.enabled = true;
+
+    // Start fading towards black.
+    FadeToBlack();
+
+    // If the screen is almost black...
+    if(guiTexture.color.a >= 0.95f)
+        // ... reload the level.
+        Application.LoadLevel(1);
+}
