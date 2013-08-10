96277992dc8e5f236f08b8280d31828f3c3b615a
pokka <ifunafu@gmail.com>
2013-08-11 03:45:08 +0800

    Tag Management
    Unity can get the GameObject reference(s) in scripts through using a Tag reference,use a script to manage these can minimize hand write errors.

---
+++ Assets/Scripts/Units/Tags.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+// A list of tag strings.
+public static var player : String = "Player";
+public static var alarm : String = "AlarmLight";
+public static var siren : String = "Siren";
+public static var gameController : String = "GameController";
+public static var mainLight : String = "MainLight";
+public static var fader : String = "Fader";
+public static var enemy : String = "Enemy";
